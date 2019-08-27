dbf39cc9978cb238ba6e5b42a2e7f057bd521c2a
at-minhtang <minh.tang@asiantech.vn>
2019-08-27 22:24:30 +0700

    done group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,9 +15,7 @@ class GroupsController < ApplicationController
     return render :new if @group.invalid?
     Group.transaction do
       @group.record.save!
-      permit_params[:device_ids].each do |id|
-        @group.record.devices_groups.create!(device_id: id)
-      end
+      insert_all_devices_groups!(permit_params[:device_ids])
     end
     redirect_to save_another_path(groups_path, new_group_path), notice: i18s(:group)
   rescue
@@ -26,12 +24,21 @@ class GroupsController < ApplicationController
 
   def edit
     @group = GroupContracts::Base.new(record: group)
+    @device_ids = @group.record.devices_groups.pluck(:device_id)
   end
 
   def update
     @group = GroupContracts::Base.new(permit_params)
     return render :edit if @group.invalid?
-    return redirect_to groups_path, notice: i18s(:group) if @group.record.save
+    Group.transaction do
+      @group.record.save!
+      device_keep_insert     = @group.device_ids
+      device_will_be_removed = @group.record.devices_groups.pluck(:device_id) - device_keep_insert
+      destroy_all_devices_groups!(device_will_be_removed)
+      insert_all_devices_groups!(device_keep_insert)
+    end
+    redirect_to save_another_path(groups_path, edit_group_path(@group.record)), notice: i18s(:group)
+  rescue
     redirect_to edit_group_path(group.id), alert: i18f(:group)
   end
 
@@ -53,5 +60,13 @@ class GroupsController < ApplicationController
 
   def devices
     @devices ||= Device.order_alphabet.includes(:area)
+  end
+
+  def insert_all_devices_groups!(device_ids)
+    device_ids.each { |device_id| @group.record.devices_groups.create!(device_id: device_id) }
+  end
+
+  def destroy_all_devices_groups!(device_ids)
+    @group.record.devices_groups.where(device_id: device_ids).destroy_all
   end
 end
