2529a176ccc291af9d0d5514e521d8e333e277ee
at-minhtang <minh.tang@asiantech.vn>
2019-08-27 21:23:53 +0700

    destroy group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_many :devices_groups
+  has_many :devices_groups, dependent: :destroy
   has_many :devices, through: :devices_groups
 
   scope :order_alphabet, -> { order(name: :asc) }
