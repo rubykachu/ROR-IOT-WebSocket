ff705460836ebccc36678e50f1cfb897c1185ebc
at-minhtang <minh.tang@asiantech.vn>
2019-08-28 17:24:14 +0700

    remove migrate

--- db/migrate/20190826025645_create_areas.rb
+++
@@ -1,9 +0,0 @@
-class CreateAreas < ActiveRecord::Migration[6.0]
-  def change
-    create_table :areas do |t|
-      t.string :name, null: false
-      t.string :slug, null: false
-      t.timestamps null: true
-    end
-  end
-end
--- db/migrate/20190826102240_create_devices_groups.rb
+++
@@ -1,8 +0,0 @@
-class CreateDevicesGroups < ActiveRecord::Migration[6.0]
-  def change
-    create_table :devices_groups do |t|
-      t.belongs_to :device, null: false, foreign_key: true
-      t.belongs_to :group, null: false, foreign_key: true
-    end
-  end
-end
